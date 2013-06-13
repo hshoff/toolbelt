f7f7293bed1254ef470a9f90416e290527a12c88
Harrison Shoff <hi@hshoff.com>
2013-06-13 13:36:27 -0700

    [reqAnimFrame] add existence check

--- requestAnimationFrame.js
+++ requestAnimationFrame.js
@@ -1,12 +1,16 @@
-window.requestAnimationFrame = (function() {
-  return
-    window.requestAnimationFrame       ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame    ||
-    window.oRequestAnimationFrame      ||
-    window.msRequestAnimationFrame     ||
+if (!window.requestAnimationFrame) {
 
-    function(callback) {
-      window.setTimeout(callback, 10000/60);
-    }
-})();
+  window.requestAnimationFrame = (function() {
+    return
+      window.requestAnimationFrame       ||
+      window.webkitRequestAnimationFrame ||
+      window.mozRequestAnimationFrame    ||
+      window.oRequestAnimationFrame      ||
+      window.msRequestAnimationFrame     ||
+
+      function(callback) {
+        window.setTimeout(callback, 10000/60);
+      }
+  })();
+
+}
